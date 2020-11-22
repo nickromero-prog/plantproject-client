5f0a60a9bfeeaf0db59f03ce3bff3e448bf1dbe5
nickromero-prog <romeronick93@gmail.com>
2020-11-21 22:34:12 -0500

    Fixed updatePot
    fixed Id in update

--- assets/scripts/pots/api.js
+++ assets/scripts/pots/api.js
@@ -37,7 +37,7 @@ const deletePot = function (data) {
 
 const updatePot = function (data) {
   return $.ajax({
-    url: config.apiUrl + '/pots/' + data.Id,
+    url: config.apiUrl + '/pots/' + data.ID,
     headers: {
       Authorization: 'Bearer ' + store.user.token
     },
